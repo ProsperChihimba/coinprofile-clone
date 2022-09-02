6697383c74628bbcaaf9b895f52e486054b22cbf
ProsperChihimba <prosperchihimba@gmail.com>
2022-09-03 02:31:23 +0300

    [Add]: Body add money buttons & balance box

--- src/main/body/index.jsx
+++ src/main/body/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import {
+    Box,
+    Text,
+    Flex,
+    Button,
+    Center
+} from '@chakra-ui/react'
 import { Sidebar } from '../../sidebar'
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { FaRegPaperPlane } from "react-icons/fa";
+import { IoIosAdd } from "react-icons/io";
 
 const Body = () => {
     return (
@@ -15,7 +24,40 @@ const Body = () => {
                 border='1px'
                 height='80vh'
             >
-                Hello
+                <Box p={45}>
+                    <Box pb='61px'>
+                        <Text as='b'>Welcome! 👋🏽</Text>
+                    </Box>
+                    <Box pb={25}>
+                        <Text as='b'>USD 1000.00</Text>
+                    </Box>
+                    <Box>
+                        <Flex gap={12}>
+                            <Box>
+                                <Flex>
+                                    <Center>
+                                        <Text mr={2}>Estimated Total balance in</Text>
+                                        <Button
+                                            rightIcon={<AiOutlineCaretDown />}
+                                            rounded='3xl'
+                                            h='30px'
+                                        >
+                                            USD
+                                        </Button>
+                                    </Center>
+                                </Flex>
+                            </Box>
+                            <Flex gap={4}>
+                                <Button leftIcon={<FaRegPaperPlane />} bg='black' color='white' variant='solid'>
+                                    Send Money
+                                </Button>
+                                <Button leftIcon={<IoIosAdd size={30} />} bg='white' color='black' variant='solid' border='1px'>
+                                    Add Money
+                                </Button>
+                            </Flex>
+                        </Flex>
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
