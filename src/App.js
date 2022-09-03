8de76fce4e3495d0e6456a4a3ca32ed008e2016c
ProsperChihimba <prosperchihimba@gmail.com>
2022-09-03 05:13:15 +0300

    [Update]: Design form contror of login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { Box, Container  } from '@chakra-ui/react'
+import { Container  } from '@chakra-ui/react'
 import Main from './main';
+import Login from './login';
 
 function App() {
   return (
     <ChakraProvider>
-      <Container maxW={1140}>
+      {/* <Container maxW={1140}>
         <Main />
-      </Container>
+      </Container> */}
+      <Login />
     </ChakraProvider>
   );
 }
