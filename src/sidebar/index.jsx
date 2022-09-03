9b4f0ec9e41f765a32532d12bd5c883cc9c3ccb2
ProsperChihimba <prosperchihimba@gmail.com>
2022-09-03 04:38:39 +0300

    Update: Side bar get help icon

--- src/sidebar/index.jsx
+++ src/sidebar/index.jsx
@@ -10,29 +10,38 @@ import {
 export const Sidebar = () => {
     return (
         <Box pt={5}>
-            <Flex direction='column' gap={8}>
+            <Flex direction='column' gap={24}>
+                <Flex direction='column' gap={8}>
+                    <Box>
+                        <Flex>
+                            <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/HouseColored.svg' />
+                            <Text as='b' pl={4}>Home</Text>
+                        </Flex>
+                    </Box>
+                    <Box>
+                        <Flex>
+                            <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/Wallet.svg' />
+                            <Text as='b' pl={4}>Balances</Text>
+                        </Flex>
+                    </Box>
+                    <Box>
+                        <Flex>
+                            <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/CreditCard.svg' />
+                            <Text as='b' pl={4}>Cards</Text>
+                        </Flex>
+                    </Box>
+                    <Box>
+                        <Flex>
+                            <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/GearSix.svg' />
+                            <Text as='b' pl={4}>Settings</Text>
+                        </Flex>
+                    </Box>
+                </Flex>
+                <Spacer />
                 <Box>
                     <Flex>
-                        <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/HouseColored.svg' />
-                        <Text as='b' pl={4}>Home</Text>
-                    </Flex>
-                </Box>
-                <Box>
-                    <Flex>
-                        <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/Wallet.svg' />
-                        <Text as='b' pl={4}>Balances</Text>
-                    </Flex>
-                </Box>
-                <Box>
-                    <Flex>
-                        <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/CreditCard.svg' />
-                        <Text as='b' pl={4}>Cards</Text>
-                    </Flex>
-                </Box>
-                <Box>
-                    <Flex>
-                        <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654551364/coinprofile%202.0/GearSix.svg' />
-                        <Text as='b' pl={4}>Settings</Text>
+                        <Image src='https://res.cloudinary.com/payourse-technologies-inc/image/upload/v1654656139/coinprofile%202.0/chat_jp2gcw.svg' />
+                        <Text as='b' pl={4}>Get help</Text>
                     </Flex>
                 </Box>
             </Flex>
