57e10eab5a7258d83b838ec21bbc0b2806de185b
ProsperChihimba <prosperchihimba@gmail.com>
2022-09-16 22:56:53 +0300

    [Update]: Profits section texts

--- src/components/web/main/parts/profits.jsx
+++ src/components/web/main/parts/profits.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text } from '@chakra-ui/react'
 
 const Profits = () => {
     return (
         <Box bg='#F79898' borderTop='2px' borderBottom='2px'>
             <Container pt='11.52rem' pb='4.8rem'>
-                Profits
+                <Box mb='35px'>
+                    <Text as='b' fontSize='3.0rem' w='490px' letterSpacing='normal' textAlign='center'>
+                        Send, spend and earn with crypto and cash
+                    </Text>
+                </Box>
+                <Box mb='35px' m='0px auto 6.38rem'>
+                    <Text textAlign='center' fontSize='18px' fontWeight='500' >
+                        Live your life on crypto. Make day-to-day spending a breeze with cash any time you need it
+                    </Text>
+                </Box>
             </Container>
         </Box>
     )
