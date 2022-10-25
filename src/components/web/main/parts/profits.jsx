90dc571137f1d0ef781f626fc46db8c8b27747c6
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-25 14:45:56 +0300

    Update: text width and profits com padding

--- src/components/web/main/parts/profits.jsx
+++ src/components/web/main/parts/profits.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Box, Container, Text, Flex, Center, Image } from '@chakra-ui/react'
+import { Box, Text, Flex, Center, Image } from '@chakra-ui/react'
 
 const Profits = () => {
     return (
         <Box bg='#F79898' borderTop='2px' borderBottom='2px'>
-            <Box pt='11.52rem' pb='4.8rem' pr='150px' pl='150px' w='100%'>
-                <Box mb='35px'>
-                    <Text as='b' fontSize='3.0rem'  w='490px' letterSpacing='normal' textAlign='center'>
-                        Send, spend and earn with crypto and cash
-                    </Text>
+            <Box pt='11.52rem' pb='10.8rem' pr='150px' pl='150px' w='100%'>
+                <Box mb='35px' w='100%'>
+                    <Center>
+                        <Text fontWeight='600' fontSize='3.0rem'  w='490px' letterSpacing='normal' textAlign='center'>
+                            Send, spend and earn with crypto and cash
+                        </Text>
+                    </Center>
                 </Box>
-                <Box m='0px auto 6.38rem'>
-                    <Text textAlign='center' fontSize='18px' fontWeight='500' >
-                        Live your life on crypto. Make day-to-day spending a breeze with cash any time you need it
-                    </Text>
+                <Box m='0px auto 6.38rem' w='100%'>
+                    <Center>
+                        <Text textAlign='center' w='490px' fontSize='18px' fontWeight='500' >
+                            Live your life on crypto. Make day-to-day spending a breeze with cash any time you need it
+                        </Text>
+                    </Center>
                 </Box>
                 <Flex gap='100px'>
                     <Box w='292px' h='297px' p='4.02rem 2.34rem' bg='white' border='1px' rounded='lg'>
