440227fdd3126bfd04b34342c5088757a06502dc
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-09 02:52:42 +0400

    Add: cards body component

--- src/components/dashboard/cards/index.jsx
+++ src/components/dashboard/cards/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {
+    Box
+} from '@chakra-ui/react'
+import Header from '../header'
+import Body from './body'
 
 const Cards = () => {
     return (
-        <div>Cards</div>
+        <Box>
+            <Header />
+            <Body />
+        </Box>
     )
 }
 
